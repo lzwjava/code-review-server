fe8ef4dd90b8290d542515fc3558e0b90bc77bd8
尼奥 <1988wangxiao@gmail.com>
2015-10-09 17:07:01 +0800

    [web] 补充 $http 全局配置的提示。

--- web-project/src/app/auth/helloSer.js
+++ web-project/src/app/auth/helloSer.js
@@ -11,7 +11,9 @@ export default ($http, $window, lcConfig, $state) => {
     getData: () => {
       return $http({
         method: 'get',
-        url: lcConfig.apiHost + '/api/hello'
+
+        // api 请求的默认host 等设置在 config http.js 中
+        url: '/api/hello'
       });
     }
   };
