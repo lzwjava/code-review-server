55814f0d2337b20edd6d2a95ba55b062a4070396
lzwjava <lzwjava@gmail.com>
2015-12-01 19:18:12 +0800

    add composer

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # code-review-server
 
 Deploy: fab -H root@reviewcode.cn deploy
+
+Install dependencies: composer.phar install
--- index.php
+++ index.php
@@ -290,3 +290,4 @@ switch (ENVIRONMENT)
  * And away we go...
  */
 require_once BASEPATH.'core/CodeIgniter.php';
+require 'vendor/autoload.php';
